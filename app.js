2ab3227ae29c3817b06aba21d8f42090a0b03457
akkolen25 <92454829+akkolen25@users.noreply.github.com>
2022-06-10 12:29:17 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+});
+
+function mOver(obj) {
+    obj.innerHTML = "Have fun cleaning!"
+  }
+  
+  function mOut(obj) {
+    obj.innerHTML = "Mouse Over Me"
+  }
